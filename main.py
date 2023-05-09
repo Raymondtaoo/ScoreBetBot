016cb78b16463ccf881d73024f4ea3f04d1ce30e
Raymond Tao <taor10@mcmaster.ca>
2023-05-09 16:27:53 -0400

    oml
    0 vs O

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import tweepy
 import keys
 
 def api():
-    auth = tweepy.0AuthHandler(keys.api_key, keys.api_secret)
+    auth = tweepy.OAuthHandler(keys.api_key, keys.api_secret)
     auth.set_access_token(keys.access_token, keys.access_token_secret)
     return tweepy.API(auth)
 
 def tweet(api: tweepy.API, message: str, image_path: str = None):
     if image_path is not None:
-        api.update_with_media(message:str, image_path:None):
+        api.update_with_media(message: str, image_path:None):
     else:
         api.update_status(message)
 
