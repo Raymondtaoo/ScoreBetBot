87e1d368882b4fc9a8ea2249d0942ce5fbdad143
Raymond Tao <123979366+Raymondtaoo@users.noreply.github.com>
2023-09-15 01:41:14 -0400

    nightly build
    untested major changes
    
    added error handling
    organized code and created modularity through functions
    implemented logging

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
 import tweepy
 import keys
+import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Load credentials from a configuration file and initialize Twitter client
 client = tweepy.Client(consumer_key = keys.api_key,
                     consumer_secret = keys.api_secret,
                     access_token = keys.access_token,
                     access_token_secret = keys.access_token_secret)
+chrome_driver_path = keys.chrome_driver_path
 
-s = Service('/Users/raymondtao/Downloads/chromedriver_mac_arm64/chromedriver')
+# Check for missing credentials
+if not all([consumer_key, consumer_secret, access_token, access_token_secret, chrome_driver_path]):
+    logger.error("Missing Twitter API credentials or Chrome driver path.")
+    exit(1)
+
+# Initialize Chrome driver
+s = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=s)
 
-driver.get("https://www.oddsshark.com/nba/odds")
+def scrape_odds(url):
+    driver.get(url)
+    names = driver.find_elements(By.CLASS_NAME, 'participant-name')
+    odds = driver.find_elements(By.CLASS_NAME, 'odds-moneyline')
 
-names = driver.find_elements(By.CLASS_NAME, 'participant-name')
-odds = driver.find_elements(By.CLASS_NAME, 'odds-moneyline')
+    moneyline = [i.text for i in odds if i.text != '- -']
+    teams = [i.text for i in names]
 
-moneyline = []
-for i in odds:
-    moneyline.append(i.text)
-moneyline = [item for item in moneyline if item != '- -']
-teams = []
-for i in names:
-    teams.append(i.text)
+    return teams, moneyline
 
+def tweet_odds(team1, team2, moneyline1, moneyline2):
+    tweet_text = f"{team1} vs. {team2}\nMoneyline: {moneyline1} {team1}, {moneyline2} {team2}"
+    client.create_tweet(text=tweet_text)
+    logger.info(f"Tweeted: {tweet_text}")
 
-for i in range(0, len(teams), 2):
-    team1 = teams[i]
-    team2 = teams[i + 1]
-    moneyline1 = moneyline[i]
+if __name__ == "__main__":
+    odds_url = "https://www.oddsshark.com/nba/odds"
+    teams, moneyline = scrape_odds(odds_url)
+
+    for i in range(0, len(teams), 2):
+        team1 = teams[i]
+        team2 = teams[i + 1]
+        moneyline1 = moneyline[i]
+        moneyline2 = moneyline[i + 1]
+
+        tweet_odds(team1, team2, moneyline1, moneyline2)
+
+    driver.quit()
     moneyline2 = moneyline[i + 1]
 
     response = client.create_tweet(text=f"{team1} vs. {team2}\nMoneyline: {moneyline1} {team1}, {moneyline2} {team2}")
